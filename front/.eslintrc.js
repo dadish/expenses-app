f2e3279765385179a61957c7983729ef89c670ca
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-08 00:43:08 +0600

    Setup Hello-World - Iteration #1

---
+++ front/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "mocha": true,
+    "es6": true
+  },
+  "plugins": [
+    "import",
+    "react",
+    "jsx-a11y",
+    "redux-saga",
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+    "import/prefer-default-export": 0,
+    "import/no-extraneous-dependencies": 0,
+    "react/jsx-filename-extension": [1, { "extensions": ["js", "jsx"] }],
+    "react/forbid-prop-types": 0,
+    "react/prop-types": [2, { "ignore": ["children"] }],
+    "redux-saga/yield-effects": 2,
+    "redux-saga/no-yield-in-race": 2,
+    "redux-saga/no-unhandled-errors": 0,
+  }
+};
--- front/README.md
+++ front/README.md
@@ -6,9 +6,37 @@ the needs of the project.
 
 ## Stack
 The main technologies that are used for this project are:
+- Node.js
 - React
 - React-Router
 - Redux
 - Redux-Saga
 - Redux-Form
 - Material-UI
+
+## Usage
+Assuming you have Node.js@lts and git@^2.7.4 installed on
+your system follow the next steps.
+
+Clone the main repo.
+```bash
+~$ git clone git@git.toptal.com:Nurguly-Ashyrov/nurguly-ashyrov-2nd-attempt.git
+```
+Go to `front` directory.
+```bash
+~$ cd front
+```
+### Development Mode
+To start the development mode use the following command.
+```bash
+~$ npm start
+```
+### Production
+To build a production app.
+```bash
+~$ npm build
+```
+To serve the production app.
+```bash
+~$ npm run start:production
+```
