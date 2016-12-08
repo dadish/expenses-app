729b741285713e56aa87288d2257a6a3c2816e97
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-08 18:11:33 +0600

    Fix the test.

--- front/src/components/Logo/index.test.js
+++ front/src/components/Logo/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import expect from 'expect';
 import { shallow } from 'enzyme';
-import Icon from 'material-ui/svg-icons/maps/directions-run';
+import Icon from 'material-ui/svg-icons/action/account-balance';
 import Logo from './';
 
 test('The Logo renders without errors', () => {
