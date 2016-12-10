e0cb2b122bbcbfd2c0d19d1a010202c261421041
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-10 20:12:01 +0600

    Set the client`s id as the default user for new expenses.

--- front/src/containers/Expenses/Item/reducer.js
+++ front/src/containers/Expenses/Item/reducer.js
@@ -1,5 +1,6 @@
 import { fromJS } from 'immutable';
 import uniqueId from 'lodash/uniqueId';
+import auth from 'auth';
 import {
   CREATE,
   EDIT_MODE_ON,
@@ -15,7 +16,7 @@ import {
 export const initialState = () => fromJS({
   cid: uniqueId('expense_'),
   id: 0,
-  user: 0,
+  user: auth.getUser().get('id'),
   amount: 0,
   date: new Date().toISOString(),
   comment: '',
