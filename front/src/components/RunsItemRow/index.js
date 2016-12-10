e60cc7336be9aace76420510c82b683917b0fea4
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-10 12:55:09 +0600

    Remove unwanted boilerplate code.

--- front/src/components/RunsItemRow/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ItemRow from 'components/ItemRow';
-
-const ExpensesItemRow = props => (
-  <ItemRow {...props} />
-);
-
-export default ExpensesItemRow;
--- front/src/components/RunsItemRow/index.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ExpensesItemRow from './';
-
-it('renders without errors', () => {
-  shallow(<ExpensesItemRow />);
-});
