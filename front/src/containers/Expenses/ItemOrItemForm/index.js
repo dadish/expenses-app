146d8e1a899547db4fc98018e4999607aeb8e1fe
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-11 19:21:56 +0600

    Set the client`s id as the default user for new expenses.

--- front/src/containers/Expenses/ItemOrItemForm/index.js
+++ front/src/containers/Expenses/ItemOrItemForm/index.js
@@ -1,24 +1,33 @@
 import React, { PropTypes, PureComponent } from 'react';
 import { Map } from 'immutable';
-import Item from '../Item';
-import { createForm } from '../ItemForm';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import Item from 'containers/Expenses/Item';
+import { createForm } from 'containers/Expenses/ItemForm';
+import { selectUser } from 'containers/App/selectors';
 
 export class ItemOrItemForm extends PureComponent {
   render() {
-    const { expense } = this.props;
+    const { expense, user } = this.props;
+    const values = user.get('role') === 300 ? expense : expense.set('user', user.get('id'));
     let item;
     if (expense.get('edit')) {
       const Form = createForm(expense.get('cid'));
-      item = (<Form initialValues={expense} />);
+      item = (<Form initialValues={values} />);
     } else {
-      item = (<Item expense={expense} />);
+      item = (<Item expense={values} />);
     }
     return item;
   }
 }
 
+export const mapStateToProps = createStructuredSelector({
+  user: selectUser(),
+});
+
 ItemOrItemForm.propTypes = {
   expense: PropTypes.instanceOf(Map),
+  user: PropTypes.instanceOf(Map),
 };
 
-export default ItemOrItemForm;
+export default connect(mapStateToProps)(ItemOrItemForm);
