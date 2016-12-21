b256263c2c2ebaa5240b21a0d6a7ba376b6c1546
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-21 16:55:44 +0600

    Change the db depending on environment to avoid production data loss.

--- back/src/config.js
+++ back/src/config.js
@@ -7,15 +7,17 @@ const db = {
 
 const testDb = {
   host: '127.0.0.1',
-  user: 'expenses_test',
+  user: 'expenses',
   password: 'expenses-mysql',
   database: 'expenses',
 };
 
+/* $lab:coverage:off$ */
 const config = {
   port: process.env.PORT || 3010,
   debug: Boolean(process.env.DEBUG),
   db: process.env.TEST ? testDb : db,
 };
+/* $lab:coverage:on$ */
 
 export default config;
