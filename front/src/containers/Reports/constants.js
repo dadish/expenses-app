07f57d49ce4b6c9229d2ae95946f87d13af699bb
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-12-21 15:18:26 +0600

    Rename the constants for Reports. Forgot to rename them after copying from Expenses.

--- front/src/containers/Reports/constants.js
+++ front/src/containers/Reports/constants.js
@@ -2,8 +2,8 @@
  * Expenses constants
  */
 
-export const SET_CURRENT_PAGE = 'app/Expenses/SET_CURRENT_PAGE';
-export const SET_ITEMS_PER_PAGE = 'app/Expenses/SET_ITEMS_PER_PAGE';
-export const SET_TOTAL_ITEMS = 'app/Expenses/SET_TOTAL_ITEMS';
-export const LIST_UPDATE_START = 'app/Expenses/LIST_UPDATE_START';
-export const LIST_UPDATE_STOP = 'app/Expenses/LIST_UPDATE_STOP';
+export const SET_CURRENT_PAGE = 'app/Reports/SET_CURRENT_PAGE';
+export const SET_ITEMS_PER_PAGE = 'app/Reports/SET_ITEMS_PER_PAGE';
+export const SET_TOTAL_ITEMS = 'app/Reports/SET_TOTAL_ITEMS';
+export const LIST_UPDATE_START = 'app/Reports/LIST_UPDATE_START';
+export const LIST_UPDATE_STOP = 'app/Reports/LIST_UPDATE_STOP';
