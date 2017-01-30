7004a6bb246630b69c38fef87a1f0f093dfbed4b
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-30 10:42:17 +0600

    Make the db credentials be populated from environment variables.

--- back/src/config.js
+++ back/src/config.js
@@ -1,15 +1,15 @@
 const db = {
-  host: '127.0.0.1',
-  user: 'expenses',
-  password: 'expenses-mysql',
-  database: 'expenses',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 };
 
 const testDb = {
-  host: '127.0.0.1',
-  user: 'expenses',
-  password: 'expenses-mysql',
-  database: 'expenses',
+  host: process.env.DB_TEST_HOST || process.env.DB_HOST,
+  user: process.env.DB_TEST_USER || process.env.DB_USER,
+  password: process.env.DB_TEST_PASS || process.env.DB_PASS,
+  database: process.env.DB_TEST_NAME || process.env.DB_NAME,
 };
 
 /* $lab:coverage:off$ */
